edaa602c12942060a327aa6912094cc42a9579ad
Vladays <tsvetk1@yandex.ru>
2025-09-19 23:11:56 +0300

    Revert "Update scraper.py"
    This reverts commit d0728ca7a05d89f588f7d8cda369d7a8e982ef5d.

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -73,22 +73,9 @@ class Config:
         Args:
             path_to_config (pathlib.Path): Path to configuration.
         """
-        self._seed_urls: list[str]
-        self._num_articles: int
-        self._headers: dict[str, str]
-        self._encoding: str
-        self._timeout: int
-        self._should_verify_certificate: bool
-        self._headless_mode: bool
+        self.path_to_config = path_to_config
         self._config_data = self._extract_config_content()
         self._validate_config_content()
-        self._seed_urls = self._config_data.seed_urls
-        self._num_articles = self._config_data.total_articles
-        self._headers = self._config_data.headers
-        self._encoding = self._config_data.encoding
-        self._timeout = self._config_data.timeout
-        self._should_verify_certificate = self._config_data.should_verify_certificate
-        self._headless_mode = self._config_data.headless_mode
         self.url_pattern = re.compile(
             r'^https://www\.kchetverg\.ru/\d{4}/\d{2}/\d{2}/[\w\-]+/?$')
 
